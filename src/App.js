7b928a388f5386b12b32e3134a7bf4fea4fb3bd0
pzync <shkhrgrv@gmail.com>
2019-07-14 11:55:50 +0530

    three card drag with blurry bg and rotate y animation done

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import { motion } from "framer-motion";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [isTapped, setIsTapped] = useState(false);
 
   return (
     <div className="App">
       <motion.div
-        className="Box"
-        whileTap={{ scale: 0.85, borderRadius: `50%` }}
+        className="Container"
         drag
         dragElastic={1}
         dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-        onDragStart={() => setValue(20)}
+        onDragStart={() => setValue(40)}
         onDragEnd={() => setValue(0)}
-        animate={{
-          scale: [1, 0.6, 1.2, 1],
-          borderRadius: [`15%`, `50%`, `50%`, `15%`],
-          rotate: [0, 180, -180, 0]
-        }}
-      />
+        onTapStart={() => setIsTapped(true)}
+        onTap={() => setIsTapped(false)}
+      >
+        <motion.div className="Box Front" />
+        <motion.div
+          className="Box Middle"
+          animate={isTapped ? { rotate: 10, y: -120 } : { rotate: 0, y: 0 }}
+          transition={{ type: "spring", stiffness: 120, damping: 14 }}
+        />
+        <motion.div
+          className="Box Back"
+          animate={isTapped ? { rotate: 20, y: -240 } : { rotate: 0, y: 0 }}
+          transition={{ type: "spring", stiffness: 120, damping: 14 }}
+        />
+      </motion.div>
       <div className="App-bg" style={{ filter: `blur(${value}px)` }} />
     </div>
   );
